2b00a3ed24678b3a86b7017cab9ee7dc5659ff0c
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-12 16:06:38 +0200

    Day 1 - Variables & Basics
    Made this basic Celsius to Fahrenheit converter

---
+++ Temperature_Converter/Temperature_Converter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Celsius to Fahrenheit converter");
+            Console.WriteLine();
+
+            Console.WriteLine("How many degrees Celsius is it? ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            temp = (temp * 9 / 5) + 32;
+
+            Console.WriteLine("It is " + temp + " degrees Fahrenheit");
+        }
+    }
+}
