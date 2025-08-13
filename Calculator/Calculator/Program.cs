72a90a8b0f3c7851d94066b573dd706713ba19b8
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-13 12:28:34 +0200

    Day 2: user input, type conversion and string concatenation with calculator app

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator app!");
+
+            Console.Write("Enter your first number: ");
+            double firstNum = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your second number: ");
+            double secondNum = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Choose your operation(+, -, *, /): ");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            if (operation == '+')
+            {
+                var output = firstNum + secondNum;
+                Console.WriteLine($"Result = {output}");
+            }
+            else if (operation == '-')
+            {
+                var output = firstNum - secondNum;
+                Console.WriteLine($"Result = {output}");
+            }
+            else if (operation == '*')
+            {
+                var output = firstNum * secondNum;
+                Console.WriteLine($"Result = {output}");
+            }
+            else if (operation == '/')
+            {
+                var output = firstNum / secondNum;
+                Console.WriteLine($"Result = {output}");
+            }
+        }
+    }
+}
