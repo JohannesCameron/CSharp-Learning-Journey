a59058c98eeeab528c22498e5328eaccef37c760
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-14 11:02:22 +0200

    Day 3: switch statements

---
+++ Day_of_the_week_checker/Day_of_the_week_checker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_of_the_week_checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number from 1 - 7: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("It is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("It is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("It is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("It is Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("It is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("It is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("It is Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+
+            }
+
+        }
+    }
+}
