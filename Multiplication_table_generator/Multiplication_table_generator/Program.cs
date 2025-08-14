115bafd12a042589e310e44f90ba437be9397ab1
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-14 11:31:49 +0200

    Day 3: While & for loops

---
+++ Multiplication_table_generator/Multiplication_table_generator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_table_generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time table generator");
+            Console.WriteLine();
+
+            bool again = true;
+
+            while (again)
+            {
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                for (int i = 1; i < 11; i++)
+                {
+                    Console.WriteLine($"{num} x {i} = {num * i}");
+                }
+                Console.WriteLine();
+                Console.Write("Do you want to do another number? (yes/no): ");
+                string input = Console.ReadLine();
+
+                if (input == "yes")
+                {
+                    again = true;
+                }
+                else if (input == "no")
+                {
+                    again = false;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input, try again!");
+                    again = true;
+                }
+                
+            }
+        }
+    }
+}
