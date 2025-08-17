06063fb2643cdef6273bba641e3cc78c8b7b5636
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-17 17:24:58 +0200

    Day 6: Getters & Setters, auto & read-only properties

---
+++ Bank_ACcount_Simulator/Bank_ACcount_Simulator/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_ACcount_Simulator
+{
+    internal class BankAccount
+    {
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set
+            {
+                balance = value;
+            }
+        }
+
+        public string OwnerName { get; set; }
+
+        public BankAccount(decimal bal, string ownerName)
+        {
+            Balance = bal;
+            OwnerName = ownerName;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
---
+++ Bank_ACcount_Simulator/Bank_ACcount_Simulator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_ACcount_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount(12000, "Cameron");
+
+            account1.Deposit(5000);
+            Console.WriteLine(account1.Balance);
+
+            account1.Withdraw(2000);
+            Console.WriteLine(account1.Balance);
+        }
+    }
+}
