46453a98132fdf8caf109c82f9c29d068d8721b2
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-17 16:51:42 +0200

    Day 6: Classes, Objects , Constructors and Basic Class Methods

---
+++ Student_profile_app/Student_profile_app/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_profile_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Cameron", 20, 0);
+            Student student2 = new Student("Hailey", 16, 11);
+            Student student3 = new Student("Xander", 15, 9);
+
+            student1.DisplayProfile();
+            student2.DisplayProfile();
+            student3.DisplayProfile();
+        }
+    }
+}
---
+++ Student_profile_app/Student_profile_app/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_profile_app
+{
+    internal class Student
+    {
+        public string Name;
+        public int Age;
+        public int Grade;
+
+        public Student(string aName, int aAge, int aGrade)
+        {
+            Name = aName;
+            Age = aAge;
+            Grade = aGrade;
+        }
+
+        public void DisplayProfile()
+        {
+            Console.WriteLine($"Student Name: {Name} \nStudent Age: {Age} \nStudent Grade: {Grade}");
+        }
+    }
+}
