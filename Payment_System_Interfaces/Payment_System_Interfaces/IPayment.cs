65d19aa1994082370fc7b4897690fe2454645d1f
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-19 18:04:46 +0200

    Day 8: Interfaces

---
+++ Payment_System_Interfaces/Payment_System_Interfaces/IPayment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment_System_Interfaces
+{
+    public interface IPayment
+    {
+        void Pay(decimal amount);
+    }
+
+    public class CreditCardPayment : IPayment
+    { 
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount:C} usiing Credit Card.");
+        }
+    }
+
+    public class PayPalPayment : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount:C} usiing PayPal.");
+        }
+    }
+
+    public class BitcoinPayment : IPayment
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount:C} usiing Bitcoin.");
+        }
+    }
+
+    public class PaymentProcessor
+    {
+        public void ProcessPayment(IPayment paymentMethod, decimal amount)
+        {
+            paymentMethod.Pay(amount);
+        }
+    }
+
+}
---
+++ Payment_System_Interfaces/Payment_System_Interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment_System_Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor processor = new PaymentProcessor();
+
+            IPayment creditCard = new CreditCardPayment();
+            IPayment paypal = new PayPalPayment();
+            IPayment bitcoin = new BitcoinPayment();
+
+            processor.ProcessPayment(creditCard, 150.00m);
+            processor.ProcessPayment(paypal, 250.50m);
+            processor.ProcessPayment(bitcoin, 0.005m);
+        }
+    }
+}
