3fde0d8ca504f8e5a911c86299ad89357028d721
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-20 13:18:56 +0200

    Day 8: OOP concepts

---
+++ Banking_App/Banking_App/IAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_App
+{
+    public interface IAccount
+    {
+        void Deposit(decimal amount);
+        void Withdraw (decimal amount);
+        decimal GetBalance();
+    }
+
+    class Account : IAccount
+    { 
+        public int accountNumber { get; set; }
+        public decimal balance { get; protected set; }
+
+        public Account(decimal Balance)
+        {
+            balance = Balance;
+        }
+
+        public void Deposit(decimal Balance)
+        {
+            balance += Balance;
+        }
+
+        public void Withdraw(decimal Balance)
+        {
+            balance -= Balance;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+
+    class SavingsAccount : Account 
+    {
+        public decimal InterestRate = 0.02m;
+
+        public SavingsAccount(decimal initialBalance) : base(initialBalance) { }
+
+        public void AddInterest()
+        {
+            balance += balance * InterestRate;
+        }
+    }
+
+
+}
---
+++ Banking_App/Banking_App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1000);
+            acc1.Deposit(1000);
+            acc1.Withdraw(500);
+            Console.WriteLine(acc1.GetBalance());
+        }
+    }
+}
