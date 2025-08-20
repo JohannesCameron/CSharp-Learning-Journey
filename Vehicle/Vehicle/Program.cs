68d377785e4389710e661543baaacc6b954b3a58
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-20 13:45:20 +0200

    Day 8: Inheritance

---
+++ Vehicle/Vehicle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Ford", 120);
+            Bike bike = new Bike("Yamaha", 160);
+            Truck truck = new Truck("Mercedes", 110);
+
+            car.Drive();
+            bike.Drive();
+            truck.Drive();
+        }
+    }
+}
---
+++ Vehicle/Vehicle/Vehicle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    public class Vehicle
+    {
+        public string brand;
+        public int topSpeed;
+
+        public Vehicle(string Brand, int TopSpeed)
+        {
+            brand = Brand;
+            topSpeed = TopSpeed;
+        }
+
+        public virtual void Drive()
+        {
+            Console.WriteLine("The vehicle is driving!");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public Car(string Brand, int TopSpeed) : base(Brand, TopSpeed) { }
+
+        public override void Drive()
+        {
+            Console.WriteLine("The car is drivng!");
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public Bike(string Brand, int TopSpeed) : base (Brand, TopSpeed) { }
+        public override void Drive()
+        {
+            Console.WriteLine("The bike is driving!");
+        }
+    }
+
+    class Truck : Vehicle 
+    {
+        public Truck(string Brand, int TopSpeed) : base (Brand, TopSpeed) { }
+        public override void Drive()
+        {
+            Console.WriteLine("The truck is driving!");
+        }
+    }
+
+}
