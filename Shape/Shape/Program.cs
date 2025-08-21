8a95da17ccf1859006552333d0f9792bdc758732
Cameron  Johannes <166021022+JohannesCameron@users.noreply.github.com>
2025-08-21 17:07:07 +0200

    Day 9: Polymorphism

---
+++ Shape/Shape/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> list = new List<Shape>();
+
+            list.Add(new Shape());
+            list.Add(new Circle());
+            list.Add(new Rectangle());
+            list.Add(new Triangle());
+
+            foreach (Shape shape in list)
+            {
+                shape.Draw();
+            }
+        }
+    }
+}
---
+++ Shape/Shape/Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    public class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a shape");
+        }
+    }
+    public class Circle : Shape
+    { 
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Rectangle");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Triangle");
+        }
+    }
+}
